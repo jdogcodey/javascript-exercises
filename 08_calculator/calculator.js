1864164dc91d172fa3ce15aa85bb3c056f3a04a6
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-07 20:22:54 +0000

    COmplete 08_calculator
    Found this one all easy until I got to the factorial where I kept
    accidentally creating endless loops.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	total = num1 + num2;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	total = num1 - num2;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(myNums) {
+let total = 0;
+for (let i = 0; i < myNums.length; i++) {
+  total += myNums[i];
+}
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function(myNums) {
+let total = 1;
+for (let i = 0; i < myNums.length; i++) {
+  total = total * myNums[i];
+}
+return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	total = Math.pow(num1, num2);
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let total = 1;
+  if (num1 >= 1) {
+    for (let i = 1; i <= num1; i++) {
+  total = total * i;
+}
+  }
+  return total;
 };
 
 // Do not edit below this line

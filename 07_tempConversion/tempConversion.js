eb03c32f508e50c138b3dd2789894bcd3828d418
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-07 19:28:36 +0000

    Complete 07_tempConversion
    Got this one first try as well. Will look at their code to see if it is
    tidier than mine but happy with this one so far.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+let newTemp = ((temp-32)*5)/9;
+let rounded = Math.round(newTemp * 10) / 10;
+return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+let newTemp = (temp*1.8)+32;
+let rounded = Math.round(newTemp * 10) / 10;
+return rounded;
 };
 
 // Do not edit below this line

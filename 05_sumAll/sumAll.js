3ad9cdd7c7101f827ba70d67b38a3fc533327ea1
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-07 18:25:56 +0000

    Completed first 4 tasks
    Wanted to upload this as I'm stuck a little at this point so can
    look back if I end up changing this dramatically.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstVal, secondVal) {
+    let rollingSum = 0;
+    if (firstVal < 0 || secondVal < 0) return 'ERROR'
+    else if (firstVal < secondVal)
+        for (i = firstVal; i <= secondVal; i++) {
+        rollingSum = rollingSum + i;
+        }
+    else for (i = secondVal; i <= firstVal; i++) {
+        rollingSum = rollingSum + i;
+        }
+    return rollingSum; 
 };
 
 // Do not edit below this line

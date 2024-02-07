0a9b206225ef3172dff17dc772e8121652bcdcd1
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-07 16:59:35 +0000

    Test 1 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+    position = value-1;
+    array.splice(position, 1)
+    return array;
+    
 };
 
 // Do not edit below this line

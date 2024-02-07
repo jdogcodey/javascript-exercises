4a27ccf8db2b7e8a2bc30847694207f8174aafd3
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-07 16:07:12 +0000

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let output = ''
+for (let testNum = 1; testNum <= num; testNum++) {
+    output += string
+}
+if (num < 0) {
+    output = 'ERROR'
+}
+return output
 };
 
 // Do not edit below this line

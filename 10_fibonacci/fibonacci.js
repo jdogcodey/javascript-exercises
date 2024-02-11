a227f45f7683641d03ce69094683222c1ae840c6
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-11 04:53:45 +0000

    Completed exercise 10_fibonacci
    Didn't take me too long to work this one out however I accidentally
    created an endless loop by making i = input in my for loop. Need to
    be more aware of stuff like that.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+let numberToAdd = 0;
 
+const fibonacci = (input) => {
+  if (input < 0) {
+    return "OOPS";
+  }
+  let counter = [0, 1];
+  for (let i = 2; i <= input; i++) {
+    lastNum = i - 1;
+    twoNumAgo = i - 2;
+    numberToAdd = counter[lastNum] + counter[twoNumAgo];
+    counter.push(numberToAdd);
+  }
+  return counter[input];
 };
+
+// Me working out the formula - x = (array reference[x-1]) + (array reference[x-2])
 
 // Do not edit below this line
 module.exports = fibonacci;

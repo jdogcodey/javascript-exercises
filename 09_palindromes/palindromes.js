22ab1e1a1cfdaee1a2fc9c2be3609387bcc487bf
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-11 03:53:46 +0000

    Completed 09_palindromes
    Took me far too long troubleshooting on the final bit of this one.
    Was stuck on how to get the loop to return true or false. When
    I finally worked it out it felt obvious but it took far too long.
    Got there in the end though and it is all learning.
    
    Looking at their answer compared to mine I have definitely
    over-complicated things however I'm not too fussed as long
    as my code does what it needs to do. Over time I am sure that
    my code will become much more efficient.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  let regularOrder = input
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, "")
+    .split("");
+  let reverseOrder = regularOrder.slice().reverse();
+  let result = true;
+  for (let i = 0; i < regularOrder.length; i++) {
+    if (regularOrder[i] !== reverseOrder[i]) {
+      result = false;
+      break;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line

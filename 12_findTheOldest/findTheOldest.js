ac3068a4acba50557038e199ad4cf0ed77038b60
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-12 20:58:57 +0000

    Finished exercise 12_findTheOldest
    Took a little while on this one as well - finding that
    these are testing my brain a little bit.
    
    Got there in the end with a completely different solution to the
    one in the solutions file.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (namesArray) {
+  for (i = 0; i < namesArray.length; i++)
+    if (!("yearOfDeath" in namesArray[i])) {
+      namesArray[i].yearOfDeath = new Date().getFullYear();
+    }
+  const ordered = namesArray.sort(function (a, b) {
+    // prettier-ignore
+    if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  return ordered[0];
 };
 
 // Do not edit below this line
